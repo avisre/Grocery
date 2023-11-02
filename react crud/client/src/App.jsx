8f9fe411ac7addb8fab5b0334871e3ef634c00d9
avisre <90497762+avisre@users.noreply.github.com>
2023-11-02 09:53:07 +0000

    Update App.jsx

--- react crud/client/src/App.jsx
+++ react crud/client/src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import grocer from '../Assets/shopping-bag.png'
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
 import './index.css';
 
@@ -10,22 +9,16 @@ function App() {
   const [num, setNum] = useState(0);
   const [list, setList] = useState([]);
   const [editMode, setEditMode] = useState({});
-  const [editQuantity, setEditQuantity] = useState(0);
-  const [editMetric, setEditMetric] = useState('');
 
   useEffect(() => {
-    Axios.get('https://cyan-mushy-panther.cyclic.app/read').then((response) => {
+    Axios.get('https://grocery-pbvd.onrender.com/read').then((response) => {
       setList(response.data);
     });
   }, []);
 
   const add = async () => {
-    await Axios.post('https://cyan-mushy-panther.cyclic.app/insert', {
-      count: count,
-      num: num,
-      metric: metric
-    });
-    const response = await Axios.get('https://cyan-mushy-panther.cyclic.app/read');
+    await Axios.post('https://grocery-pbvd.onrender.com/insert', { count: count, num: num, metric: metric });
+    const response = await Axios.get('https://grocery-pbvd.onrender.com/read');
     setList(response.data);
     setCount('');
     setNum(0);
@@ -34,18 +27,11 @@ function App() {
 
   const updateFood = async (id) => {
     try {
-      await Axios.put('https://cyan-mushy-panther.cyclic.app/update', {
-        id: id,
-        name: name,
-        quantity: editQuantity,
-        metric: editMetric
-      });
-      const response = await Axios.get('https://cyan-mushy-panther.cyclic.app/read');
+      await Axios.put('https://grocery-pbvd.onrender.com/update', { id: id, name: name });
+      const response = await Axios.get('https://grocery-pbvd.onrender.com/read');
       setList(response.data);
       setEditMode({ ...editMode, [id]: false });
       setName('');
-      setEditQuantity(0);
-      setEditMetric('');
     } catch (error) {
       console.error('Error updating food:', error);
     }
@@ -53,8 +39,8 @@ function App() {
 
   const deleteFood = async (id) => {
     try {
-      await Axios.delete(`https://cyan-mushy-panther.cyclic.app/delete/${id}`);
-      const response = await Axios.get('https://cyan-mushy-panther.cyclic.app/read');
+      await Axios.delete(`https://grocery-pbvd.onrender.com/delete/${id}`);
+      const response = await Axios.get('https://grocery-pbvd.onrender.com/read');
       setList(response.data);
     } catch (error) {
       console.error('Error deleting food:', error);
@@ -64,15 +50,15 @@ function App() {
   return (
     <>
       <div className='Header'>
-        <h1>Grocers</h1><img src={grocer} alt='Grocery icon' />
-      </div>
+        <h1>Grocers</h1><img src='/Assets/kitchen.svg'></img> </div>
       <div className="App">
+
         <label htmlFor="name">Enter Food Item</label>
         <input
           type="text"
           id="name"
           value={count}
-          placeholder='Foodstuff'
+          placeholder='Foodstuff '
           onChange={(e) => {
             setCount(e.target.value);
           }}
@@ -88,8 +74,10 @@ function App() {
         />
         <label htmlFor="pet-select">Metrics</label>
         <select
+
           className='Custom'
           name="pets"
+          id="pet-select"
           value={metric}
           onChange={(e) => {
             setMetric(e.target.value);
@@ -98,7 +86,6 @@ function App() {
           <option value=''>Select One</option>
           <option value="Kg">Kg</option>
           <option value="L">L</option>
-
           <option value="Nos">Nos</option>
         </select>
         <button onClick={add}>Add</button>
@@ -109,7 +96,8 @@ function App() {
         {list.map((d, index) => {
           return (
             <div className='Item' key={d._id}>
-              {`${index + 1}.`} {`${d.foodName[0].toUpperCase() + d.foodName.slice(1)}-`}
+              {`${index + 1}.`} {`${d.foodName[0].toUpperCase() + d.foodName.slice(1)}`} {d.quantity}
+              {d.metrics}
               {editMode[d._id] ? (
                 <>
                   <input
@@ -120,32 +108,10 @@ function App() {
                       setName(e.target.value);
                     }}
                   />
-                  <input
-                    type="number"
-                    placeholder="Quantity..."
-                    value={editQuantity}
-                    onChange={(e) => {
-                      setEditQuantity(e.target.value);
-                    }}
-                  />
-                  <select
-                    className='sm'
-                    name="pets"
-                    value={editMetric}
-                    onChange={(e) => {
-                      setEditMetric(e.target.value);
-                    }}
-                  >
-                    <option value=''>Select</option>
-                    <option value="Kg">Kg</option>
-                    <option value="L">L</option>
-                    <option value="Nos">Nos</option>
-                  </select>
                   <button onClick={() => updateFood(d._id)}>Save</button>
                 </>
               ) : (
                 <>
-                  {d.quantity} {d.metrics}
                   <button onClick={() => setEditMode({ ...editMode, [d._id]: true })}>Edit</button>
                   <button onClick={() => deleteFood(d._id)}>Delete</button>
                 </>
